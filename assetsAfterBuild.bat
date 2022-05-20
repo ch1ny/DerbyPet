9825d57ae0e89ea783689dd180187f070db4fde6
德布罗煜 <1056317718@qq.com>
2022-05-20 22:37:40 +0800

    v2.2.0:bookmark:

--- assetsAfterBuild.bat
+++ assetsAfterBuild.bat
@@ -1,3 +1,3 @@
 copy /y ".\builtPackage.json" ".\build\package.json"
-copy /y ".\main.js" ".\build\main.js"
+echo d|xcopy /y /E ".\electron" ".\build\electron"
 echo d|xcopy /y /E ".\public\electronAssets" ".\build\electronAssets"
